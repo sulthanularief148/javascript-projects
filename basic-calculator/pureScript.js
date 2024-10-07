f955648624073f75b21af9ce0ba4f3b0530024fb
sulthanularief148 <sulthanularief148@gmail.com>
2024-10-07 15:39:07 +0530

    Calculator design added through the js

--- basic-calculator/pureScript.js
+++ basic-calculator/pureScript.js
@@ -10,7 +10,6 @@ script.onload = () => {
 
 }
 
-
 title.textContent = "Pure JS";
 head.appendChild(title);
 head.appendChild(script)
@@ -21,10 +20,10 @@ body.classList.add("bg-[rgba(184,207,141,0.65)]")
 // Main Wrapper
 const calculatorWrapper = document.createElement("div");
 calculatorWrapper.id = "calculator";
-calculatorWrapper.classList.add("bg-[rgba(0,0,0,0.8)]", "flex", "gap-10", "absolute", "left-1/2", "top-1/2", "transform", "-translate-x-1/2", "-translate-y-1/2", "w-full", "md:w-4/12", "p-8", "flex-col", "text-center", "shadow-2xl", "rounded-lg");
+calculatorWrapper.classList.add("bg-[rgba(0,0,0,0.8)]", "flex", "gap-4", "absolute", "left-1/2", "top-1/2", "transform", "-translate-x-1/2", "-translate-y-1/2", "w-full", "md:w-5/12", "p-8", "flex-col", "text-center", "shadow-2xl", "rounded-lg");
 body.appendChild(calculatorWrapper);
 const header = document.createElement("h1");
-header.classList.add("text-2xl", "font-bold")
+header.classList.add("text-2xl", "font-bold", "text-white")
 header.textContent = "Basic calculator";
 calculatorWrapper.appendChild(header)
 
@@ -46,17 +45,12 @@ const createInputField = (id, placeholder, name) => {
     return input;
 }
 
-const value1 = createInputField("first-num", "Enter The  Number", "num1");
-const value2 = createInputField("second-num", "Enter The  Number", "num2");
-// value1.addEventListener('input', () => console.log(Number(value1.value)));
-// value2.addEventListener('input', () => console.log(Number(value2.value)));
-
-
+const value1 = createInputField("first-num", "Enter The  Number...", "num1");
 
 // Operator selection
 const Operator = document.createElement("select");
 
-Operator.classList.add("h-8", "w-full", "rounded-lg", "p-1", "outline-none");
+Operator.classList.add("h-8", "w-full", "rounded-lg", "outline-none");
 ["+", "-", "*", "/"].forEach(op => {
     const option = document.createElement("option");
     option.value = op;
@@ -64,8 +58,14 @@ Operator.classList.add("h-8", "w-full", "rounded-lg", "p-1", "outline-none");
     Operator.appendChild(option);
 });
 console.log(Operator.options[Operator.selectedIndex].value)
-calculatorWrapper.appendChild(Operator);
+fields.appendChild(Operator);
 document.replaceChild(html, document.documentElement);
+
+const value2 = createInputField("second-num", "Enter The  Number...", "num2");
+// value1.addEventListener('input', () => console.log(Number(value1.value)));
+// value2.addEventListener('input', () => console.log(Number(value2.value)));
+
+
 
 
 // Result display
@@ -101,8 +101,9 @@ const calculator = () => {
 // Add calculate button
 const calculateButton = document.createElement("button");
 calculateButton.textContent = "Calculate";
-calculateButton.classList.add("bg-blue-500", "text-white", "rounded-lg", "p-2");
 calculateButton.addEventListener('click', calculator);
+calculateButton.classList.add("bg-blue-500", "text-white", "rounded-lg", "p-2", "hover:bg-[coral]", "transition", "ease-linear", "duration-200"
+);
 calculatorWrapper.appendChild(calculateButton);
 
 // Input field 1
